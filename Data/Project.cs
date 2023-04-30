9da8b54e87b077c38bbc373fd1975b7044e71953
Zac Kilburn <kilburn.zachary@gmail.com>
2023-04-30 18:02:33 -0400

    Init Commit

---
+++ Data/Project.cs
@@ -0,0 +1,31 @@
+﻿using ZacharyKilburn_BlazorWASM.Pages;
+
+namespace ZacharyKilburn_BlazorWASM.Data
+{
+    public class Project
+    {
+        public string ProjectTitle { get; set; } = string.Empty;
+        public string ProjectDescription { get; set; } = string.Empty;
+        public string ProjectHref { get; set; } = string.Empty;
+       
+    }
+    public class ProjectLoader
+	{
+		public List<Project> projects { get; set; } = new();
+		public void LoadProjects()
+        {
+            if (projects.Count == 0)
+            {
+                Project p = new Project();
+                p.ProjectTitle = "Scoreboard";
+                p.ProjectHref = "Scoreboard";
+                p.ProjectDescription = "A custom scoreboard with bluetooth and manual score entry featuring an Android application, custom PCB designs and lots of 3D printing";
+                projects.Add(p);
+            }
+        }
+        public Project? GetProjectByHref(string href)
+        {
+            return projects.Where(p=> p.ProjectHref == href).FirstOrDefault();
+        }
+    }
+}
---
+++ wwwroot/JavaScript.js
@@ -0,0 +1,71 @@
+﻿function setupCollapsibleChevron(markactive) {
+    var coll = document.getElementsByClassName("collapsible-chevron");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        if (coll[i].getAttribute('listener') !== 'true') {
+            coll[i].setAttribute('listener', 'true');
+            coll[i].addEventListener("click", function () {
+
+                var icon = this.querySelector("span");
+                icon.classList.toggle('oi-plus');
+                icon.classList.toggle('oi-minus');
+                this.classList.toggle("active");
+
+                var content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+
+            });
+
+        }
+    }
+}
+function CollapsibleExpand(markactive) {
+    var coll = document.getElementsByClassName("collapsible-chevron");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+
+        var icon = coll[i].querySelector("span");
+        if (icon.classList.contains('oi-plus')) {
+            icon.classList.toggle('oi-plus');
+            icon.classList.toggle('oi-minus');
+            coll[i].classList.toggle("active");
+        }
+
+        var content = coll[i].nextElementSibling;
+        if (content.style.display === "block") {
+            
+        } else {
+            content.style.display = "block";
+        }
+
+    }
+}
+
+function CollapsibleCollapse(markactive) {
+    var coll = document.getElementsByClassName("collapsible-chevron");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+
+        var icon = coll[i].querySelector("span");
+        if (icon.classList.contains('oi-minus')) {
+            icon.classList.toggle('oi-plus');
+            icon.classList.toggle('oi-minus');
+            coll[i].classList.toggle("active");
+        }
+
+        var content = coll[i].nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+
+        } else {
+        }
+
+    }
+}
