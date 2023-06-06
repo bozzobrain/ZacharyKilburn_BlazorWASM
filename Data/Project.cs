ef38c6977a84a4f5cf79a7022d0492af724fa11d
Z <kilburn.zachary@gmail.com>
2023-06-06 09:07:59 -0400

    Updated resume - formatting changes - added projects

--- Data/Project.cs
+++ Data/Project.cs
@@ -1,4 +1,5 @@
 ﻿using Blazor3D.Maths;
+using System.Diagnostics;
 using ZacharyKilburn_BlazorWASM.Pages;
 
 namespace ZacharyKilburn_BlazorWASM.Data
@@ -8,7 +9,8 @@ namespace ZacharyKilburn_BlazorWASM.Data
 		public string ProjectTitle { get; set; } = string.Empty;
 		public string ProjectDescription { get; set; } = string.Empty;
 		public string ProjectHref { get; set; } = string.Empty;
-		public string ProjectMainImage { get; set; } = string.Empty;
+        public string ProjectDate { get; set; } = string.Empty;
+        public string ProjectMainImage { get; set; } = string.Empty;
 		public string CollapsedImage { get; set; } = string.Empty;
 		public string ExplodingImage { get; set; } = string.Empty;
         public string CollapsingImage { get; set; } = string.Empty;
@@ -17,6 +19,7 @@ namespace ZacharyKilburn_BlazorWASM.Data
 		public Vector3 CameraPosition { get; set; } = new Vector3();
         public Vector3 CameraLook { get; set; } = new Vector3();
         public Vector3 ScaleFactor { get; set; } = new Vector3();
+        public string DownloadLink { get; set; } = string.Empty;
 
     }
 	public class ProjectLoader
@@ -25,7 +28,8 @@ namespace ZacharyKilburn_BlazorWASM.Data
 		{
 			ProjectTitle = "Scoreboard",
 			ProjectHref = "projects/Scoreboard",
-			ProjectDescription = "A custom scoreboard with bluetooth and manual score entry featuring an Android application, custom PCB designs and lots of 3D printing",
+            ProjectDate = "2021",
+            ProjectDescription = "A custom scoreboard with bluetooth and manual score entry featuring an Android application, custom PCB designs and lots of 3D printing",
 			ProjectMainImage = "Scoreboard-Iso.png",
 			//CollapsedImage = "ScoreboardCollapsed.bmp",
 			//ExplodingImage = "ScoreboardExploding.gif",
@@ -41,10 +45,11 @@ namespace ZacharyKilburn_BlazorWASM.Data
 
 		public static readonly Project FireworkProject = new()
 		{
-			ProjectTitle = "Fireworks Mortor Launcher",
+			ProjectTitle = "Fireworks Mortar Launcher",
 			ProjectHref = "projects/Fireworks",
-			ProjectDescription = "A 12 shot custom fireworks mortor launcher with bluetooth controlled app, automated timing, quick reloading connections, custom PCB designs and 3D printed enclosures.",
-			ProjectMainImage = "FireworksMortor-Iso.png",
+            ProjectDate = "2021",
+            ProjectDescription = "A 12 shot custom fireworks mortar launcher with bluetooth controlled app, automated timing, quick reloading connections, custom PCB designs and 3D printed enclosures.",
+			ProjectMainImage = "FireworksMortar-Iso.png",
 			ThreeDeeModel = "FireworkMortarRack.obj",
 			ThreeDeeModelTexture = "FireworkMortarRack.mtl",
 			CameraPosition = new Vector3(400, 200, 0),
@@ -56,31 +61,57 @@ namespace ZacharyKilburn_BlazorWASM.Data
 		{
 			ProjectTitle = "200lb Combat Robot",
 			ProjectHref = "projects/Combot",
-			ProjectDescription = "Senior design project from The University of Akron. Competing in the 200lb weight class, the robot featured autonomous operation with optical distance sensors nested within its rotating weapon. The robot was capable of operating after having been flipped upside down. The autonomous operation was possible with a neural network trained in MATLAB.",
+            ProjectDate = "2018",
+            ProjectDescription = "Senior design project from The University of Akron. " +
+			"With drive wheels located outside of the body on top and bottom, the robot was capable of driving after being inverted. " +
+			"Competing in the 200lb weight class, the robot was praised as the first combat robot to feature autonomous operation." +
+			"Nested within the exterior rotating weapon, 8 uni-directional LIDAR sensors gathered data about the robots position within the arena. " +
+			"The sensors were timed to aquire measurements while the weapon windows were not obscuring the sensor view." +
+			"Measurement data was then combined into robot postition data via a neural network, trained in Matlab, running in an onboard microcontroller.",
 			ProjectMainImage = "CombatRobot.png",
 			ThreeDeeModel = "CombatRobot.obj",
 			ThreeDeeModelTexture = "CombatRobot.mtl",
 			CameraPosition = new Vector3(600, 300, 0),
 			CameraLook = new Vector3(0, 50, 0),
 			ScaleFactor = new Vector3(500, 500, 500),
+			DownloadLink = "Autonomous Combat Robot Report.pdf",
 		};
 
-		public static readonly Project SumoRobot = new()
+        public static readonly Project SumoRobot = new()
+        {
+            ProjectTitle = "Sumo Bot Competition",
+            ProjectHref = "projects/Sumo",
+            ProjectDate = "2013",
+            ProjectDescription = "",
+            ProjectMainImage = "sumo-bot2.png",
+        };
+		public static readonly Project EDMPowerSupply = new()
 		{
-			ProjectTitle = "Sumo Bot Compeition",
-			ProjectHref = "projects/Sumo",
+			ProjectTitle = "EDM Power Supply",
+			ProjectHref = "projects/EDMPSU",
+			ProjectDate = "2013",
 			ProjectDescription = "",
 			ProjectMainImage = "sumo-bot2.png",
 		};
+		public static readonly Project BMEBike = new()
+		{
+			ProjectTitle = "Outreach - Assisted Bike Brake",
+			ProjectHref = "projects/BMEBike",
+			ProjectDate = "2018",
+            ProjectDescription = "",
+            ProjectMainImage = "BMEBike.jpg",
+        };
+        
 
 
-		public List<Project> projects { get; set; } = new()
+        public List<Project> projects { get; set; } = new()
 		{
 			ScoreboardProject,
 			FireworkProject,
 			CombatRobot,
 			SumoRobot,
-		};
+            BMEBike,
+        };
 		public Project? GetProjectByHref(string href)
 		{
 			return projects.Where(p => p.ProjectHref.Contains(href)).FirstOrDefault();
