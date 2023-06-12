21b9d5769505659f08d044f10709c1a012355502
Z <kilburn.zachary@gmail.com>
2023-06-12 09:15:26 -0400

    Boards page formatting work - Combat bot and project text capabilities

--- Data/Project.cs
+++ Data/Project.cs
@@ -145,11 +145,17 @@ namespace ZacharyKilburn_BlazorWASM.Data
             ProjectTitle = "200lb Combat Robot",
             ProjectHref = "projects/Combot",
             ProjectDate = "2018",
-            ProjectDescription = "Senior design project from The University of Akron. " +
-            "With drive wheels located outside of the body on top and bottom, the robot was capable of driving after being inverted. " +
-            "Competing in the 200lb weight class, the robot was praised as the first combat robot to feature autonomous operation." +
+            ProjectDescription = "Senior Capstone project from The University of Akron. The robot received the name Nigel (Thornberry) for no obvious reason. Nigel competed in linkstart href=http://robogames.net/index.php hrefend RoboGames linkend, a robotics competition in San Mateo, California. \n\n" +
+            "hstart Design hend" +
+            "Nigel had drive wheels that protruded outside of the body on top and bottom, meaning that the robot was capable of driving after being inverted, which often occurs when other robots try to flip their opponent into the air. " +
+            "Yielding a 60lb spinning outer shell as an impacting weapon, two impacting hammers layered with solid steel provided a rigid and powerful weapon for Nigel. " +
+            "The hammers would reach a max speed of 400mph when the shell was fully energized. " +
+            "Driving the shell was the 8HP, Angi 95R motor, commonly used for EV vehicle applications. " +
+            "While this posed serious control challenges, there was even more to Nigel than just a punishing weapon design. \n\n" +
+            "hstart Autonomous Operation hend" +
+            "Competing in the 200lb weight class, Nigel was praised as the first combat robot in the competition to feature autonomous operation. " +
             "Nested within the exterior rotating weapon, 8 uni-directional LIDAR sensors gathered data about the robots position within the arena. " +
-            "The sensors were timed to aquire measurements while the weapon windows were not obscuring the sensor view." +
+            "The sensors were timed to aquire measurements while the weapon windows were not obscuring the sensor view. " +
             "Measurement data was then combined into robot postition data via a neural network, trained in Matlab, running in an onboard microcontroller.",
             ProjectMainImage = "CombatRobot.png",
        
@@ -188,7 +194,9 @@ namespace ZacharyKilburn_BlazorWASM.Data
             ProjectTitle = "EDM Power Supply",
             ProjectHref = "projects/EDMPSU",
             ProjectDate = "2013",
-            ProjectDescription = "Designed as a contract engineer, the EDM PSU was a variable AC/DC power supply intended for use with a CNC controlled manufacturing machine.",
+            ProjectDescription = "Designed as part of a contract engineering assignment, the EDM PSU was conceived as a variable AC/DC power supply intended for use with a CNC controlled manufacturing machine." +
+            "Providing constant current / constant voltage over the range of 10-100V and 5mA - 10A. \n\n" +
+            "While never fully validated by the client, and not short of its limitations, it was a challenging project that functioned well during bench testing.",
             ProjectMainImage = "EDMPSU.png",
            
             SubPages = new List<ProjectSubPage>()
